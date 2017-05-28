a81b89c7b8053466b8907971fbc7440858c1238f
user.name = 'Yale Work <user.email = 'yale.work@gmail.com>
2017-05-27 20:20:30 -0700

    Show grid

--- app.jsx
+++ app.jsx
@@ -14,6 +14,22 @@ const VALID_CHANGES = {
   'e': ['n', 's'],
 }
 
+const Row = ({i, snakeSet, width}) => {
+  let row = [];
+  for (let j = 0; j < width; j++) {
+    if (snakeSet.has([i, j].toString())) {
+      row.push((<div className="snake box"></div>));
+    } else {
+      row.push((<div className="empty box"></div>));
+    }
+  }
+  return (
+    <div className="row">
+      {row}
+    </div>
+  );
+};
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,17 +39,40 @@ export default class App extends React.Component {
         this.direction = DIRECTION_MAP[e.which];
       }
     });
-    this.curPos = (this.props.height, parseInt(this.props.width / 2, 10));
-    this.snakeCoords = [];
+    this.state = {
+      curPos: [this.props.height, parseInt(this.props.width / 2, 10)],
+      height: this.props.height,
+      width: this.props.width,
+    }
+    this.state['snakeQueue'] = [this.state.curPos];
+    this.state['snakeSet'] = new Set(this.state.snakeQueue.toString());
+    this.createGrid = this.createGrid.bind(this);
+    this.createRow = this.createRow.bind(this);
   }
 
   validDirection(newDir) {
     return VALID_CHANGES[this.direction].includes(newDir);
   }
 
+  createRow(i) {
+    return (
+      <Row i={i} snakeSet={this.state.snakeSet} width={this.state.width} />
+    );
+  }
+
+  createGrid() {
+    let rows = [];
+    for (let i = 0; i < this.state.height; i++) {
+      rows.push(this.createRow(i))
+    }
+    return rows;
+  }
+
   render() {
     return(
-      <div className='testing'>HI!</div>
+      <div className="grid">
+        {this.createGrid()}
+      </div>
     );
   }
 }
